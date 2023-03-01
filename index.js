cdec6fac97b841ff8fc0a0b2e1a7ccf046a6e447
DikshakAdhikari <ddikshakk@gmail.com>
2023-03-02 02:22:33 +0530

    11- when single reducer used

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const bindActionCreators= redux.bindActionCreators
 
 const CAKE_ORDERED= 'CAKE_ORDERED'
 const CAKE_RESTOKED= 'CAKE_RESTOKED'
+const ICECREAM_ORDERED= 'ICECREAM_ORDERED'
+const ICECREAM_RESTOCKED= 'ICECREAM_RESTOCKED'
 
 
 function orderCake(){
@@ -24,9 +26,25 @@ function restockCake( qty=1){
     }
 }
 
+
+function orderIceCream(qty=1){
+    return{
+        type: ICECREAM_ORDERED,
+        payload: qty
+    }
+}
+
+function restockIceCream(qty=1){
+    return{ 
+        type:ICECREAM_RESTOCKED,
+    payload: qty
+}
+}
+
+
 const initialState= {
     numOfCakes:10,
-    
+    numOfIceCreams:20
 }
 
 
@@ -43,10 +61,23 @@ const reducer= (state= initialState, action) => {
                 ...state,
                 numOfCakes: state.numOfCakes + action.payload
             }
+           
+    
+    case ICECREAM_ORDERED:
+            return {
+                ...state, 
+                numOfIceCreams: state.numOfIceCreams - 1
+            }
+        case ICECREAM_RESTOCKED:
+            return{
+                ...state,
+                numOfIceCreams: state.numOfIceCreams + action.payload
+            }
             default:
                 return state
     }
 }
+
 
 const store= createStore(reducer)
 console.log('Initial state', store.getState());
@@ -59,10 +90,25 @@ const unsubscribe = store.subscribe(()=> console.log('update state ', store.getS
 // store.dispatch(orderCake())
 // store.dispatch(restockCake(3))
 
-const actions= bindActionCreators({ orderCake, restockCake}, store.dispatch)
+const actions= bindActionCreators({ orderCake, restockCake ,orderIceCream, restockIceCream}, store.dispatch)
 actions.orderCake()
 actions.orderCake()
 actions.orderCake()
 actions.restockCake(3)
+actions.orderIceCream()
+actions.orderIceCream()
+actions.restockIceCream(2)
 
 unsubscribe()
+
+
+/*Output
+Initial state { numOfCakes: 10, numOfIceCreams: 20 }
+update state  { numOfCakes: 9, numOfIceCreams: 20 }
+update state  { numOfCakes: 8, numOfIceCreams: 20 }
+update state  { numOfCakes: 7, numOfIceCreams: 20 }
+update state  { numOfCakes: 10, numOfIceCreams: 20 }
+update state  { numOfCakes: 10, numOfIceCreams: 19 }
+update state  { numOfCakes: 10, numOfIceCreams: 18 }
+update state  { numOfCakes: 10, numOfIceCreams: 20 }
+*/
