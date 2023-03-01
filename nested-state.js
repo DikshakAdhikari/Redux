c8881bc9e7b27b8c4aa765d657ac3cd37065d52e
DikshakAdhikari <ddikshakk@gmail.com>
2023-03-02 03:12:09 +0530

    13- nested state i.e. address object here. Witihout using Immer function

---
+++ nested-state.js
@@ -0,0 +1,43 @@
+const redux= require('redux')
+
+const initialState= {
+    name: 'Dikshak',
+    address: {
+        street: 'Urban Bnglr St',
+        city: 'Bangalore',
+        state:'Karnataka'
+    }
+}
+
+const STREET_UPDATED= 'STREET_UPDATED'
+const updateStreet= (street) => {
+    return {
+        type: STREET_UPDATED,
+        payload:street
+    }
+}
+
+const reducer= (state= initialState, action) => {
+    switch(action.type){
+
+    case STREET_UPDATED:
+            return {
+                ...state, 
+                address: {
+                    ...state.address,
+                    street: action.payload
+                },
+            }
+            default:
+                return state
+    }
+}
+
+const store= redux.legacy_createStore(reducer)
+console.log('Initial state', store.getState());
+
+const unsubscribe = store.subscribe(()=> console.log('Updated state ', store.getState()))
+
+store.dispatch(updateStreet('Haldwani St'))
+
+unsubscribe()
